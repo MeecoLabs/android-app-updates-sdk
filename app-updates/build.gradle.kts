35c73bcadeded1269a07ffbe4fd6c184b51f66a0
Dustin Steiner <124467+alopix@users.noreply.github.com>
2026-08-28 14:03:48 +0100

    update compile sdk

--- app-updates/build.gradle.kts
+++ app-updates/build.gradle.kts
@@ -9,9 +9,7 @@ plugins {
 android {
     namespace = "eu.meecolabs.appupdates"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
